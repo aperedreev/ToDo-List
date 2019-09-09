276026cddf5236ef5847c7829e8fc4c9e4f53531
Aleksey Peredreev <peredreev.aleksey@gmail.com>
2019-09-10 00:40:36 +0400

    Initial Commit

---
+++ ToDo List/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDo List
+//
+//  Created by Aleksey Peredreev on 10.09.2019.
+//  Copyright © 2019 Aleksey Peredreev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
